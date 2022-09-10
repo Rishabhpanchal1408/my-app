977a07fcda630309eb4b6ada1b1768e8ca69485f
Rishabhpanchal1408 <106129990+Rishabhpanchal1408@users.noreply.github.com>
2022-09-10 12:12:30 +0530

    SideBarComponent Created

---
+++ src/Components/Sidebar.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <>
+    <div className='sidebar'>
+        <ul>
+            <li>
+                <a className='a' href="#HOME">HOME</a>
+            </li>
+            <li>
+                <a className='a'href="#HOME">SHOWCASES</a>
+            </li>
+            <li>
+                <a className='a'href="#HOME">SERIVCES</a>
+            </li>
+            <li>
+                <a className='a'href="#HOME">DESIGNERS</a>
+            </li>
+            <li>
+                <a className='a'href="#HOME">PACKAGES</a>
+            </li>
+            <li>
+                <a className='a'href="#HOME">CONTACT</a>
+            </li>
+        </ul>
+    </div>
+    
+    </>
+  )
+}
+
+export default Sidebar
